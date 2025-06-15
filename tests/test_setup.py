159a366bbb41fb0460ce3ddd9bfed77add8fa432
Atanu Sarkar <mratanusarkar@gmail.com>
2025-06-15 16:34:31 +0530

    fix: black formatting errors

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -2,7 +2,9 @@ def test_working_unit_test():
     """Test that the unit test is working."""
     assert True
 
+
 def test_tickersnap_import():
     """Test that the package can be imported."""
     import tickersnap
+
     assert tickersnap is not None
