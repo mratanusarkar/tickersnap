a93f62f905b096304590fd71359eaa530061e6a6
Atanu Sarkar <mratanusarkar@gmail.com>
2025-06-23 18:19:13 +0530

    remove: py-module since this is a package

---
+++ tickersnap/__init__.py
@@ -0,0 +1,11 @@
+"""
+Tickersnap - Stock data snapshots from Tickertape IN
+
+A Python library for accessing Indian stock market data including:
+
+- Market Mood Index (MMI) data
+- List of all stocks and ETFs
+- Stock scorecard analysis data
+"""
+
+__version__ = "0.0.3"
